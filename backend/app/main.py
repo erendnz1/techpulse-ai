922c66e88a76dc4eaa6fb2e2888eab80a7622359
EREN DENİZ <erenden224284@gmail.com>
2026-07-08 11:15:59 +0300

    feat: setup PostgreSQL and SQLAlchemy connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
+from sqlalchemy import text
+
+from app.database.session import engine
+
 from fastapi import FastAPI
+
 
 app = FastAPI(
     title="TechPulse AI API",
@@ -14,3 +19,13 @@ def root():
         "status": "Running 🚀",
         "message": "Welcome to TechPulse AI API"
     }
+
+@app.on_event("startup")
+def startup():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        print("✅ PostgreSQL bağlantısı başarılı.")
+    except Exception as e:
+        print("❌ Veritabanı bağlantı hatası:")
+        print(e)
