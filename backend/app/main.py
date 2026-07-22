b45037861c6ec56f2f1138216351b577d6ebefad
EREN DENİZ <erenden224284@gmail.com>
2026-07-22 20:31:54 +0300

    Allow Vercel frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,28 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
+
 from app.database.base import Base
 from app.database.session import engine
+
 from app.models.user import User
 from app.models.news import News
 from app.models.user_preferences import UserPreferences
 from app.models.notification import Notification
+
 from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 from app.api.news import router as news_router
 from app.api.user_preferences import router as preferences_router
-from app.services.scheduler_service import scheduler
 from app.api.notifications import router as notifications_router
 from app.api.dashboard import router as dashboard_router
 from app.api.admin import router as admin_router
+
+from app.services.scheduler_service import scheduler
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
@@ -46,21 +53,26 @@ async def lifespan(app: FastAPI):
         scheduler.shutdown()
         print("🛑 News scheduler durduruldu.")
 
+
 app = FastAPI(
     title="TechPulse AI API",
     version="1.0.0",
     description="AI-powered software technology monitoring platform",
     lifespan=lifespan
 )
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://techpulse-ai-opal.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root():
@@ -75,7 +87,7 @@ def root():
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(news_router)
-app.include_router(preferences_router) 
+app.include_router(preferences_router)
 app.include_router(notifications_router)
 app.include_router(dashboard_router)
 app.include_router(admin_router)
